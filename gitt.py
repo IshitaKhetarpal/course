4c669ac091ad1fd2e2fe621938283aa7911a76ac
IshitaKhetarpal <46920442+IshitaKhetarpal@users.noreply.github.com>
2020-02-24 13:38:54 +0530

    Add files via upload

---
+++ gitt.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+#Importing Packages
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
